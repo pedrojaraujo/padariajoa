db730731df1000faefca8fc0d99cc129af1063b3
pedrojaraujo <pedroa990@gmail.com>
2023-12-02 23:31:14 -0300

    add resposive mobile for banner and about

--- js/responsive.js
+++ js/responsive.js
@@ -1,16 +1,29 @@
 function reposicaoElementos() {
   const screenWidth = window.innerWidth;
 
-  if (screenWidth >= 576 && screenWidth <= 768) {
+  if (screenWidth >= 320 && screenWidth <= 480) {
     const logoContainer = document.querySelector(".logo__container");
     const listContainerNavDir = document.querySelector(
       ".list__container__nav__dir"
     );
+
+    const aboutPicture = document.querySelector(".about__picture");
+    const textAbout = document.querySelector(
+      ".titex__about__container"
+    );
+
     if (logoContainer && listContainerNavDir) {
       const nav = document.querySelector(".nav__container");
       nav.insertBefore(logoContainer, listContainerNavDir);
     }
+
+    if (aboutPicture && textAbout) {
+      const about = document.querySelector(".container__about");
+      about.insertBefore(textAbout, aboutPicture);
+    }
   } 
+
+
   
 }
 
