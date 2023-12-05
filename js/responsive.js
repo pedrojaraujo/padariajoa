b5f9e3eb1012e751a6993f777308c610aa86d0e6
pedrojaraujo <pedroa990@gmail.com>
2023-12-05 18:29:24 -0300

    add responsive medium style

--- js/responsive.js
+++ js/responsive.js
@@ -1,23 +1,30 @@
+// Váriaveis que contém os elementos necessários para serem manipulados do HTML
+const logoContainer = document.querySelector(".logo__container");
+const listContainerNavDir = document.querySelector(
+  ".list__container__nav__dir"
+);
+const aboutPicture = document.querySelector(".about__picture");
+const textAbout = document.querySelector(".titex__about__container");
+const buttonSandwich = document.querySelector(".container__sandwichMenu");
+const nav = document.querySelector(".nav__container");
+const about = document.querySelector(".container__about");
+
+// Função que manipula ordem do HTML de acordo com a resolução da tela
 function reposicaoElementos() {
   const screenWidth = window.innerWidth;
 
   if (screenWidth >= 320 && screenWidth <= 681) {
-    const logoContainer = document.querySelector(".logo__container");
-    const listContainerNavDir = document.querySelector(
-      ".list__container__nav__dir"
-    );
-
-    const aboutPicture = document.querySelector(".about__picture");
-    const textAbout = document.querySelector(".titex__about__container");
-
-    const buttonSandwich = document.querySelector(".container__sandwichMenu");
-
     if (logoContainer && listContainerNavDir && buttonSandwich) {
-      const nav = document.querySelector(".nav__container");
       nav.insertBefore(logoContainer, listContainerNavDir);
       nav.insertBefore(buttonSandwich, listContainerNavDir);
     }
 
+    if (aboutPicture && textAbout) {
+      about.insertBefore(textAbout, aboutPicture);
+    }
+  }
+
+  if (screenWidth >= 641 && screenWidth <= 1007) {
     if (aboutPicture && textAbout) {
       const about = document.querySelector(".container__about");
       about.insertBefore(textAbout, aboutPicture);
@@ -25,5 +32,6 @@ function reposicaoElementos() {
   }
 }
 
+//Acionador da função de acordo com o carregamento e redimensionamento da página.
 window.addEventListener("load", reposicaoElementos);
 window.addEventListener("resize", reposicaoElementos);
